62446ac0a50d9db644749e8b1b0948acdfcfb779
mayra@mumuki.org <mayra@mumuki.org>
2021-03-11 16:47:33 +0000

    Mumuki Export on 2021-03-11 16:47:33 +0000

--- 00007_Es fraccion mayor a uno/test..js
+++ 00007_Es fraccion mayor a uno/test..js
@@ -1,4 +1,4 @@
-it("Si hacemos esFraccionMayorAUno('1/2') debería retornar false"), function() {
+it("Si hacemos esFraccionMayorAUno('1/2') debería retornar false", function() {
 let resultado = esFraccionMayorAUno('1/2')
 assert.equal(resultado, false,`esFraccionMayorAUno('1/2') debería retornar false pero fue ${resultado}`);
 })
