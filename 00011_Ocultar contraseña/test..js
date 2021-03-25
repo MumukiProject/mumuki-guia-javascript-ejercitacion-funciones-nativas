0a780f164cc15e9b220a822f3078e3e84dd09a56
mayra@mumuki.org <mayra@mumuki.org>
2021-03-25 16:46:00 +0000

    Mumuki Export on 2021-03-25 16:46:00 +0000

--- 00011_Ocultar contraseña/test..js
+++ 00011_Ocultar contraseña/test..js
@@ -1,6 +1,6 @@
 it("Si hacemos ocultarContrasenia(123456) debería retornar '******'", function() {
 let resultado = ocultarContrasenia(123456)
-assert.equal(resultado, '******',`ocultarContrasenia(123456) debería retornar '******' pero fue ${resultado}`);
+assert.equal(resultado, ******,`ocultarContrasenia(123456) debería retornar '******' pero fue ${resultado}`);
 })
 
 it("Si hacemos ocultarContrasenia(111222333) debería retornar '*********'", function() {
