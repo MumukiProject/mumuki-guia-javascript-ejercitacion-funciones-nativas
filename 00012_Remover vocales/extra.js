6e8f58769f957a9e72ecec5bef23d3de5aa6f5c3
mayra@mumuki.org <mayra@mumuki.org>
2021-03-25 16:54:57 +0000

    Mumuki Export on 2021-03-25 16:54:57 +0000

--- 00012_Remover vocales/extra.js
+++ 00012_Remover vocales/extra.js
@@ -1,3 +1,5 @@
-function esVocal(letra){
+function esVocal(letra)/*<elipsis-for-student@*/{
   return letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u'
-}
+}/*@elipsis-for-student>*/
+
+
