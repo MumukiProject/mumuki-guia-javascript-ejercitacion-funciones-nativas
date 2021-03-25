39e378efdf7a07a24d1831a364fcb0d58da0ed1a
mayra@mumuki.org <mayra@mumuki.org>
2021-03-25 15:55:40 +0000

    Mumuki Export on 2021-03-25 15:55:40 +0000

--- 00006_Burlarse/test..js
+++ 00006_Burlarse/test..js
@@ -1,4 +1,4 @@
-it('Si hacemos burlarse('programar es dificil') debería retornar prigimir is dificil', function() {
+it("Si hacemos burlarse('programar es dificil') debería retornar 'prigimir is dificil'", function() {
 let resultado = burlarse('programar es dificil')
 assert.equal(resultado, 'prigimir is dificil',`burlarse debería retornar 'prigimir is dificil' pero fue ${resultado}`);
 })
