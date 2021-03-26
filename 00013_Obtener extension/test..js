10bc262a2f9f536ebdc07875c32e357e6e2bea7b
mayra@mumuki.org <mayra@mumuki.org>
2021-03-26 13:50:07 +0000

    Mumuki Export on 2021-03-26 13:50:07 +0000

--- 00013_Obtener extension/test..js
+++ 00013_Obtener extension/test..js
@@ -8,7 +8,3 @@ let resultado = obtenerExtension('index.html')
 assert.equal(resultado, 'html',`obtenerExtension('index.html') debería retornar 'html' pero fue ${resultado}`);
 })
 
-it("Si hacemos obtenerExtension('notas.txt') debería retornar 'txt'", function() {
-let resultado = obtenerExtension('notas.txt')
-assert.equal(resultado, 'txt',`obtenerExtension('notas.txt') debería retornar 'txt' pero fue ${resultado}`);
-})
